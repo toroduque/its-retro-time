700ed669c8d35d8ffa8ff7f85ee0315e89b5ac32
toroduque <toro.duque@gmail.com>
2019-07-04 00:28:14 +1000

    Fix close review board modal button

--- src/scenes/ReviewBoard/ReviewBoard.jsx
+++ src/scenes/ReviewBoard/ReviewBoard.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Modal from 'components/Modal'
+import Button from 'components/Button'
+import { RoomContext } from 'contexts'
 import Icon from 'components/Icon'
 import Sidebar from './components/Sidebar'
 import MainBoard from './components/MainBoard'
 import * as styled from './styled'
 
 const ReviewBoard = () => {
+    const roomContext = useContext(RoomContext)
+
+    const closeReviewBoard = () => {
+        roomContext.dispatch({type: 'HIDE_REVIEW_BOARD'})
+    }
+
     return (
         <Modal>
             <styled.ReviewBoardWrapper>
                 <styled.HeaderWrapper>
                     <h3>Review</h3>
-                    <Icon glyph="close" size="12" />
+                    <Button text icon onClick={closeReviewBoard}>
+                        <Icon glyph="close" size="12" />
+                    </Button>
                 </styled.HeaderWrapper>
                 <styled.ContentWrapper>
                     <Sidebar />
