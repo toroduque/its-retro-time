328ce1c348d5f4ff99d4e6577d1a5882fa41e822
toroduque <toro.duque@gmail.com>
2019-09-04 23:26:04 +1000

    Removed unused code

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,29 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import { useTransition, animated } from 'react-spring'
+import React from 'react'
 import Logo from 'components/Logo'
 import * as styled from './styled'
 
-const Loading = () => {
-    const [ isToggling, setIsToggling ] = useState(false)
-
-    useEffect(() => {
-        setIsToggling(true)
-        return setIsToggling(false)
-    }, [])
-    
-    const transitions = useTransition(isToggling, null, {
-        from: { position: 'absolute', opacity: 0 },
-        enter: { opacity: 1 },
-        leave: { opacity: 0 },
-        config: {duration: 50}
-    })
-    
-    return (
-        <styled.LoadingWrapper>
-            <Logo iconOnly width="40"/>
-            <div>... Loading</div>
-        </styled.LoadingWrapper>
-    )
-}
+const Loading = () =>  (
+    <styled.LoadingWrapper>
+        <Logo iconOnly width="40"/>
+        <div>... Loading</div>
+    </styled.LoadingWrapper>
+)
 
 export default Loading
