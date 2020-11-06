e19557b08b810e4bcff971f0097f5775779f87f9
Daniel Toro <toro.duque@gmail.com>
2020-11-07 10:17:17 +1100

    Update copywrite of About and Readme pages --- Run  to update packages

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # It's Retro Time
 An easy-to-use tool to help your team run restrospective meetings in a simple and effective way.
+Feel free to use it at [retrotime.app](https://retrotime.app)
+
+## Why?
+This idea was born to solve a very simple problem: How to run the retrospective meetings for our engineering team in an easy way while some of the team members were working remotely.
--- src/scenes/About/About.jsx
+++ src/scenes/About/About.jsx
@@ -10,8 +10,8 @@ function About() {
                 <styled.AboutContent>
                     <h1>About</h1>
                     <p>RetroTime is a free and open-source tool to run restrospectives meetings for remote teams.</p>
-                    <p>This idea was born to solve a very simple problem. How to run the retrospective meetings for our engineering team in an easy way while some of the team members were working remotely.</p>
-                    <p>We want to share this simple and easy-to-use tool with the world</p>
+                    <p>This idea was born to solve a very simple problem: How to run the retrospective meetings for our engineering team in an easy way while some of the team members were working remotely.</p>
+                    <p>I wanted to share this simple and easy-to-use tool with anyone who wants to use it.</p>
                 </styled.AboutContent>
             </styled.ContentWrapper>
         </styled.AboutWrapper>
